a71eceed9f37b791577a80cbbc565af6444061d5
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2022-03-07 00:02:21 +0100

    commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "mySongFunc": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "myOpApi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { API } from 'aws-amplify'
+import { useEffect, useState } from 'react'
+import { Songs } from './interfaces'
 
 function App() {
+  const [songs, updateSongs] = useState<Songs>()
+
+  const getData = async () => {
+    try {
+      const data = await API.get('myOpApi', '/songs', {})
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      test
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
