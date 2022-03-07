05f1b36f1ddb7a61bd6bceb808751caf6049e68d
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2022-03-07 15:17:02 +0100

    testing out with DynamoDB for the first time

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,40 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "mysongfunc": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "mysongapi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    },
+    "auth": {
+        "operations0d90506d": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "mysongsdb": {
+            "Name": "string",
+            "Arn": "string",
+            "StreamArn": "string",
+            "PartitionKeyName": "string",
+            "PartitionKeyType": "string",
+            "SortKeyName": "string",
+            "SortKeyType": "string",
+            "Region": "string"
+        }
+    }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,58 @@
 import { API } from 'aws-amplify'
 import { useEffect, useState } from 'react'
+import * as RB from 'react-bootstrap'
 import { Songs, Jobs } from './interfaces'
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import { convertToObject } from 'typescript';
 
 function App() {
 
+  const [songs, updateSongs] = useState({
+    title: '',
+    artist: '',
+    album: ''
+  })
+
+  async function getData() {
+    try {
+      const data = await API.get('mysongapi', '/songs/title', {})
+      console.log(data)
+    } catch (error) { console.log(error) }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
 
   return (
     <div className="App">
+      <RB.Row className='justify-content-center'>
+        <RB.Col className='mt-5' md={5}>
+          <RB.Form>
+            <RB.FormGroup>
+              <RB.FormControl
+                value={songs.title}
+                placeholder="Song title"
+                onChange={(e) => updateSongs({ ...songs, title: e.target.value })} />
+            </RB.FormGroup>
 
+            <RB.FormGroup>
+              <RB.FormControl
+                value={songs.title}
+                placeholder="Song artist name"
+                onChange={(e) => updateSongs({ ...songs, artist: e.target.value })} />
+            </RB.FormGroup>
+
+            <RB.FormGroup>
+              <RB.FormControl
+                value={songs.title}
+                placeholder="Song album"
+                onChange={(e) => updateSongs({ ...songs, album: e.target.value })} />
+            </RB.FormGroup>
+          </RB.Form>
+        </RB.Col>
+      </RB.Row>
     </div>
   )
 }
